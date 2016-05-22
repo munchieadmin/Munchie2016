4dd99e4f93666b878d538e91495c5d9109736cad
munchieCORBIN <corbin@munchiemeets.com>
2016-05-22 14:49:29 -0700

    Initial Commit

---
+++ Munchie2016/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Munchie2016
+//
+//  Created by Corbin Benally on 5/22/16.
+//  Copyright © 2016 Munchie Meets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
